fe8b3dfed2f2529ae0be1b99c1b6350f5fd192bc
ErnieBuyco <buycoernie@gmail.com>
2020-02-17 18:25:41 +0800

    convert_to_jsx

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import GlobalStyle from '../styles/Global';
+
+const App = () => (
+  <div id="app">
+    <GlobalStyle />
+    <h1>Test</h1>
+  </div>
+);
+
+export default App;
